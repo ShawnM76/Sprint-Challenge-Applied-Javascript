2a444ee7944ae3fef2d417200547a6861729c636
Shawn McManus <xsvfan76@gmail.com>
2019-06-21 11:12:06 -0600

    finished-project-but-weird-bug

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -16,7 +16,7 @@ class TabLink {
       this.cards = document.querySelectorAll('.card');
     }else {
       // else if `all` is false, only select the cards with matching this.tabData values
-      this.cards = this.tabData;
+      this.cards = document.querySelectorAll(`.card[data-tab='${this.tabData}']`);
     }
     
 
